435eb1120ce9a22748b680264d5d689c5a328c94
Lindsay Evans <linz@linz.id.au>
2009-05-06 20:49:06 +1000

    Implementing partials; adding edit; breaking form out into partial

--- mgmt/mgmt.rb
+++ mgmt/mgmt.rb
@@ -16,23 +16,63 @@ get '/libraries/new' do
     erb :'libraries/new'
 end
 
-put '/libraries/new' do
-    @library = Library.new params
+put '/libraries' do
+    @library = Library.new params[:library]
 
     if @library.save
-	redirect_to '/'
+	redirect '/'
     else
 	# Error
         erb :'libraries/new'
     end
-
 end
 
 
+get '/libraries/:id' do
+    @library = Library.find params[:id]
+   erb :'libraries/edit'
+end
+
+
+put '/libraries/:id' do
+    @library = Library.find params[:id]
+
+    @library.update_attributes params[:library]
+
+    if @library.save
+	redirect '/'
+    else
+	# Error
+        erb :'libraries/edit'
+    end
+end
+
 helpers do
 
-    def link_to(what) # TODO: pretty much everything
-	return '<a href="/libraries/'+what[:id].to_s+'/edit">'+what.name+'</a>'
+    # Usage: partial :foo    
+    def partial(page, options={})
+	erb page, options.merge!(:layout => false)
     end
 
+    def link_to(what) # TODO: pretty much everything
+	return '<a href="/libraries/'+what[:id].to_s+'">'+what.name+'</a>'
+    end
+
+end
+
+# filters
+before do
+  # Params hash
+  new_params = {}
+  params.each_pair do |full_key, value|
+    this_param = new_params
+    split_keys = full_key.split(/\]\[|\]|\[/)
+    split_keys.each_index do |index|
+      break if split_keys.length == index + 1
+      this_param[split_keys[index]] ||= {}
+      this_param = this_param[split_keys[index]]
+   end
+   this_param[split_keys.last] = value
+  end
+  request.params.replace new_params
 end
