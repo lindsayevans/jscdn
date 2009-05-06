445628d37d0de8e98904dda92627cc04129889ef
Lindsay Evans <linz@linz.id.au>
2009-05-06 19:35:28 +1000

    Adding libraries to dashboard

--- mgmt/mgmt.rb
+++ mgmt/mgmt.rb
@@ -5,6 +5,7 @@ require 'init'
 get '/' do
     @distribution_servers = DistributionServer.all :order => :name
     @dns_servers = DnsServer.all :order => :name
+    @libraries = Library.all :order => :name
     erb :dashboard
 end
 
