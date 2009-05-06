ba2a28ba323f8330f031800c939459b5a67b287e
Lindsay Evans <linz@linz.id.au>
2009-05-06 20:25:01 +1000

    Adding new lib form

--- mgmt/mgmt.rb
+++ mgmt/mgmt.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'init'
 
+# Dashboard
 get '/' do
     @distribution_servers = DistributionServer.all :order => :name
     @dns_servers = DnsServer.all :order => :name
@@ -9,10 +10,28 @@ get '/' do
     erb :dashboard
 end
 
+# Libraries
+get '/libraries/new' do
+    @library = Library.new
+    erb :'libraries/new'
+end
+
+put '/libraries/new' do
+    @library = Library.new params
+
+    if @library.save
+	redirect_to '/'
+    else
+	# Error
+        erb :'libraries/new'
+    end
+
+end
+
 
 helpers do
 
-    def link_to(what)
+    def link_to(what) # TODO: pretty much everything
 	return '<a href="/libraries/'+what[:id].to_s+'/edit">'+what.name+'</a>'
     end
 
